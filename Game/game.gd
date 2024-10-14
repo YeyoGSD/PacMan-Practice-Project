164f09eff95e8dd3d24193418c85578f312cf328
YeyoGSD <YeyoGSD@proton.me>
2024-10-13 20:11:37 -0700

    Agrega tilemap navegable e inicio del fantasma rojo

--- Game/game.gd
+++ Game/game.gd
@@ -1,4 +1,10 @@
 extends Node2D
 
+@onready var player:Player = $Player
+@onready var red_ghost:Ghost = $Ghosts/RedGhost
+
 func _on_player_collected_pellet():
-	print("pellet collected")
+	pass
+
+func _on_red_timer_timeout() -> void:
+	red_ghost.to_chase_state(player.global_position)
